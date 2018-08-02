d40808ac6d6df708749df32d0b4290210e2dadd6
usamahamid <usamahamid007@gmail.com>
2018-08-02 11:18:12 +0530

    Introduced Response model

--- fluxc/src/main/java/org/wordpress/android/fluxc/model/DomainAvailabilityModel.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/model/DomainAvailabilityModel.kt
@@ -1,11 +1,11 @@
 package org.wordpress.android.fluxc.model
 
 class DomainAvailabilityModel(
-    val product_id: Int?,
-    val product_slug: String?,
-    val domain_name: String?,
+    val productId: Int?,
+    val productSlug: String?,
+    val domainName: String?,
     val status: String?,
     val mappable: String?,
     val cost: String?,
-    val supports_privacy: Boolean = false
+    val supportsPrivacy: Boolean = false
 )
